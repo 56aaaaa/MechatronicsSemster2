5779bd80cf834193a3d97f4475d044fa8808874b
andrew <andrew.hughes.101@outlook.com>
2019-03-25 11:47:08 +0000

    Howard's automated door
    Howards automated door code written in week 8 lab session

--- doorNode/doorNode.ino
+++ doorNode/doorNode.ino
@@ -1,14 +1,42 @@
-//smart door system node
+#include <Servo.h>
 
 bool door = false;
 bool mat = false;
 int occupants = 0;
 bool RFID = false;
+int buttonState=0;
+
+int buttonPin=2;
+int servoPin=12;
+
+Servo servodoor;
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(buttonPin,INPUT);
+  servodoor.attach(servoPin);
+  servodoor.write(0);
 }
 
 void loop() {
-
+  buttonState=digitalRead(buttonPin);
+  Serial.println(buttonState);
+  Serial.println(door);
+  if(buttonState==HIGH && !door)
+  {
+  for(int i=0;i<90;i++)
+  {
+    servodoor.write(i);
+    delay(20);}
+    delay(3000);
+    door=true;
+  } 
+  if(door)
+  {
+    for(int i=90;i>0;i--)
+    {
+      servodoor.write(i);
+      delay(20);}
+    } 
+    door=false;
 }
