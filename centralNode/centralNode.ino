3e4ad1bfae57a6cecb90e324e589e8ffd42f5cfe
andrew <andrew.hughes.101@outlook.com>
2019-03-28 15:50:37 +0000

    Added temperature control
    Controller to determine temperature value and adjust system parameters accordingly has been added.

--- centralNode/centralNode.ino
+++ centralNode/centralNode.ino
@@ -1,19 +1,64 @@
 //Humidity, temperature sensor node
 
+float getTemp();  //method to get the temperature from the temperature sensor in celsius
+
+void heatingOn();   //methods to turn on and off both the heater and the fan to control the systems environment temperature 
+void heatingOff();
+void fanOn();
+void fanOff();
+
+void tempController(int minHeat, int maxHeat, float temp);    //control loop to ensure that the temperature will be within a suitable range
+
 int humidity = 0;
-int temp = 0;
 bool alarm = false;
-int maxHeat = 25;
-int minHeat = 18;
+
 int minHumid = 45;
 int maxHumid = 55;
-bool fan = false;
-bool heater = false;
 
+bool fan = false;   //indicator to check if the fan is on or off
+bool heater = false;  //indicator to check if the heater is on or off
+  
 void setup() {
-
+  Serial.begin(9600);
 }
 
 void loop() {
+  int maxHeat = 25;
+  int minHeat = 18;
+  
+  float temp = getTemp();
 
+  tempController(minHeat,maxHeat,temp);
+  
 }
+
+float getTemp() {
+  int rawVoltage = analogRead(0);   //reads in value from analogue pin a0
+  return (rawVoltage/1024.0)*500;    //converts the raw voltage value into a temperature in celsius and returns it
+}
+
+void tempController(int minHeat, int maxHeat, float temp) {
+  if(temp < minHeat) {
+    if(fan) {
+      void fanOff();
+      fan = false;
+    }
+    void heatingOn();
+    heater = true;
+  }
+  else if(temp > maxHeat) {
+    if(heater) {    //ensure heater is off
+      void heatingOff();
+      heater = false;
+    }
+    void fanOn();
+    fan = true;
+  }
+  else {
+    void heatingOff();  
+    void fanOff();
+    heater = false;
+    fan = false;
+  }
+}
+
